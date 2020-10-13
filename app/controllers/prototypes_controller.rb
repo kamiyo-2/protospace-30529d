9ad8e3506d199befade31d8870179ceae8ee8f04
kamiyo-2 <71446248+kamiyo-2@users.noreply.github.com>
2020-10-13 11:45:30 +0900

    edit修正

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -27,6 +27,9 @@ class PrototypesController < ApplicationController
 
   def edit
     @prototype = Prototype.find(params[:id])
+    if @prototype.user != current_user
+      redirect_to root_path
+    end
   end
 
   def update
